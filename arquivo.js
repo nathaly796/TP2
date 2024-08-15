e34fef93e3596149f4a3b6cca2b28a539cc4599e
nathaly796 <143623710+nathaly796@users.noreply.github.com>
2024-08-15 15:01:09 -0300

    aula do dia 15/08

---
+++ arquivo.js
@@ -0,0 +1,9 @@
+const express = require('express')
+const app = express()
+app.use(express.urlencoded({ extended: true }))
+app.set('view engine', 'ejs')
+app.get('/', (request, response) => {
+
+response.render("arquivo")
+})
+app.listen(8080)
